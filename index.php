a06181855eae6dfe3b360e24dad305579b938742
MuhNiswar <97176029+MuhNiswar@users.noreply.github.com>
2022-02-13 21:16:13 +0700

    saya sudah ubah

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 <body>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo nostrum incidunt amet, facilis tenetur sunt vitae ut eos, rem modi, veritatis consectetur fugit et hic assumenda eius qui eaque ratione!</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur minima ad possimus, excepturi vel nostrum reiciendis amet dicta vero odio consectetur expedita quaerat deleniti optio deserunt corporis at perspiciatis. Unde, placeat dicta eaque quidem ex porro dolorum repellendus sint. Cum odio sit fugiat architecto fugit dicta asperiores, expedita est neque!</p>
 </body>
 
 </html>
