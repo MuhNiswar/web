7d829ca6012162be907133fa1df53a2c69e90b82
MuhNiswar <97176029+MuhNiswar@users.noreply.github.com>
2022-02-13 21:15:01 +0700

    tessssssssssss

---
+++ index.php
@@ -0,0 +1,7 @@
+<html>
+
+<body>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo nostrum incidunt amet, facilis tenetur sunt vitae ut eos, rem modi, veritatis consectetur fugit et hic assumenda eius qui eaque ratione!</p>
+</body>
+
+</html>
