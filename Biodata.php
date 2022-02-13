b72361dbede548b464c9c682da1b19c5ed6de678
MuhNiswar <97176029+MuhNiswar@users.noreply.github.com>
2022-02-13 21:21:19 +0700

    saya menambahkan file biodata

---
+++ Biodata.php
@@ -0,0 +1,8 @@
+<html>
+
+<p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolores quod ipsa possimus. Ut, quas quam optio laborum culpa iusto omnis quibusdam neque repudiandae doloribus? Perferendis quis veritatis dolorum eveniet.
+
+</p>
+
+</html>
