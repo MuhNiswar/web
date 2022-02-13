8d66e1845ffe086934c49c3d747c45ee2110ae28
MuhNiswar <97176029+MuhNiswar@users.noreply.github.com>
2022-02-13 21:29:21 +0700

    saya menambahkan nama

--- Biodata.php
+++ Biodata.php
@@ -1,8 +1,7 @@
 <html>
 
 <p>
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolores quod ipsa possimus. Ut, quas quam optio laborum culpa iusto omnis quibusdam neque repudiandae doloribus? Perferendis quis veritatis dolorum eveniet.
-
-</p>
-
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolores quod ipsa possimus. Ut, quas quam optio laborum culpa iusto omnis quibusdam neque repudiandae doloribus? Perferendis quis veritatis dolorum eveniet.</p>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis atque tenetur, nobis autem facilis, aut a iure nam officiis nisi, odit impedit. Amet repudiandae minus quae delectus et aspernatur earum?</p>
+muhniswar
 </html>
